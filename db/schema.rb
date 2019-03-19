9b5b692eb203ff7a144c0db4972933ae3a8f37bf
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-19 13:04:18 +0100

    Changement cartes dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_112745) do
+ActiveRecord::Schema.define(version: 2019_03_19_102744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_112745) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.boolean "admin", default: false, null: false
+    t.integer "total_amount", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
