a03373625a73b7d6220247f48a2bd993f4aaa7e1
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-20 18:54:32 +0100

    fix bug

--- app/helpers/navbar_style_helper.rb
+++ app/helpers/navbar_style_helper.rb
@@ -1,5 +1,5 @@
 module NavbarStyleHelper
   def navbar_style
-    params[:controller] == "devise/sessions" || current_page?(root_path) || params[:controller] == "suscriptions"
+    params[:controller] == "devise/sessions" || current_page?(root_path) || params[:controller] == "suscriptions" || params[:controller] == "devise/registrations"
   end
 end
