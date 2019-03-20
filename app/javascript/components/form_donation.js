150549532a4094919fb965cfe0c856f4b3c406a2
Nicolas Desbonnets <nicolas.desbonnets@hec.edu>
2019-03-20 18:50:12 +0100

    changed buttons

--- app/javascript/components/form_donation.js
+++ app/javascript/components/form_donation.js
@@ -1,11 +1,13 @@
-var input_donation = document.querySelector("#user_plan_id")
+var input_donation = document.querySelectorAll("input[type=radio]")
 var params_donation = document.querySelector("#plan_params")
 //var form_donation = document.querySelector("#edit_donation")
 
 const initFormDonation = () => {
-  input_donation.addEventListener("change", (event) => {
-    params_donation.setAttribute("value", input_donation.value);
-  })
+  input_donation.forEach(function(input) {
+    input.addEventListener("click", (event) => {
+      params_donation.setAttribute("value", input.value);
+    });
+  });
 };
 
 export{initFormDonation};
